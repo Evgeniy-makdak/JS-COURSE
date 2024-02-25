a54282da1bd46730e39ebff46caea461e0d70b76
Evgeniy <58253815+Evgeniy-makdak@users.noreply.github.com>
2024-02-25 17:51:34 +0300

    Add files via upload
    solved a new problem

--- archive.js
+++ archive.js
@@ -3184,3 +3184,24 @@ const str = 'how can mirrors';
 
 //                                    ****************************************
 
+// Напишите функцию, которая принимает массив из 10 целых чисел (от 0 до 9) и возвращает строку этих чисел в виде номера телефона.
+
+// Пример:
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+// function createPhoneNumber (numbers) {
+//   const phoneNumberPrefix = numbers.splice(0, 3).join('');
+//   const operatorCode = numbers.slice(0, 3).join('');
+//   const lastNumbers = numbers.splice(3, numbers.length).join('')
+//   return `(${phoneNumberPrefix}) ${operatorCode}-${lastNumbers}`;
+// };
+
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+//                                    ****************************************
+
+// дан массив случайных чисел, найдите сумму двух наименьших чисел из этого массива
+
+function isSummOfMinimum(randArr) {
+
+}
